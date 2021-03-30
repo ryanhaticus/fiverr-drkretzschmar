852fee1f1de71c8d4d1de5cc4d6e8d1fa1c9f12e
Ryan Huellen <ryan@getpassly.com>
2021-03-30 10:02:27 -0500

    -

--- pages/index.tsx
+++ pages/index.tsx
@@ -286,11 +286,8 @@ const Index = () => {
             <div className='hidden lg:block  absolute top-0 bottom-0 left-3/4 w-screen'></div>
             <div className='mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none'>
               <div>
-                <h2 className='text-base text-lightBlue-600 font-semibold tracking-wide uppercase'>
-                  Impressum
-                </h2>
                 <h3 className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>
-                  Dr. Kretzschmar
+                  Impressum
                 </h3>
               </div>
             </div>
